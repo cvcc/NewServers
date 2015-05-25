8411b9f6b7f84875c9e928caab45f7283739cede
Soufiane Mghanen <smghanen@outlook.com>
2015-05-25 20:26:55 +0000

    Revert "sum"
    This reverts commit 8617dd44148fac1ba136fcd92e5ce40befad95b0.

--- server.js
+++
@@ -1,7 +0,0 @@
-var $ = require('./node_modules/jquerygo/lib/jquery.go.js');
-$.visit('https://www.google.com', function(){
-  $.waitForPage(function(){
-    $.capture(__dirname + '/screenshot.png');
-    $.close();
-  });
-});
